cc0bddaae9c5f5e18a1284d952712e51c7633231
Paan Kaur Riives <paan.kaur@gmail.com>
2026-08-16 17:03:33 +0300

    content: add a sentence

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           name="I am Paan Kaur Riives"
           phone="+372 5625 8379"
           email="paan.kaur@gmail.com"
-          description="Looking to begin my first job in the IT field. I am a fast learner and a hard worker. I am looking for a position where I can grow and learn new skills."
+          description={`Looking to begin my first job in the IT field. I am a fast learner and a hard worker. I am looking for a position where I can grow and learn new skills. Here will appear my future projects!`}
           link="CV-Paan-Kaur-Riives.pdf"
           linkText="Open my CV"
 />
