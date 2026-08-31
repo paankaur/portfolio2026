8fab4c569f8ba8f9af55fec24eb34a205cffca88
Paan Kaur Riives <paan.kaur@gmail.com>
2026-08-31 22:47:31 +0300

    fix: no ugly text anymore

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -1,12 +1,15 @@
 
 import { Link } from "react-router"
+import { Box } from "lucide-react"
 
 
 
 export default function ProjectsCard() {
   return (
     <div>
-        <Link to="/cube-sim" className="text-blue-500 hover:underline text-center">Cube Algorithm Simulator</Link>
+        <Link to="/cube-sim" className="text-blue-500 hover:text-blue-400 transition-colors" title="Cube Algorithm Simulator">
+          <Box size={24} />
+        </Link>
     </div>
   )
 }
