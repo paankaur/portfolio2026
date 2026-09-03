76015c28e9797b8801670467af2e07334acab8b3
Paan Kaur Riives <paan.kaur@gmail.com>
2026-09-03 09:51:09 +0300

    style: improve Cube Simulator link layout

--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -1,15 +1,17 @@
-
-import { Link } from "react-router"
-import { Box } from "lucide-react"
-
-
+import { Link } from "react-router";
+import { Box } from "lucide-react";
 
 export default function ProjectsCard() {
   return (
     <div>
-        <Link to="/cube-sim" className="text-blue-500 hover:text-blue-400 transition-colors" title="Cube Algorithm Simulator">
-          <Box size={24} />
-        </Link>
+      <Link
+        to="/cube-sim"
+        className="text-blue-500 hover:text-blue-400 transition-colors flex flex-row items-center"
+        title="Cube Algorithm Simulator"
+      >
+        <Box size={24} />
+        <span className="ml-1">Cube Simulator</span>
+      </Link>
     </div>
-  )
+  );
 }
